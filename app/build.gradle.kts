fa3d853155b8d8627a2c046773efb95355fda47b
Aleksey Cherepanov <alexej.cherepanov@gmail.com>
2026-03-12 15:57:36 +0700

    Specify jvm version for jitpack ci

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.13.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.10.0")
-    implementation("androidx.activity:activity-ktx:1.12.4")
+    implementation("androidx.activity:activity-ktx:1.13.0")
     implementation("com.squareup.retrofit2:retrofit:3.0.0")
     implementation("com.squareup.okhttp3:okhttp:5.3.2")
     implementation("com.squareup.retrofit2:converter-gson:3.0.0")
